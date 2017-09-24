b7c290242238b48f7d122135fdb068ace8190c95
Steven Tupper <10thElement@gmail.com>
2017-09-25 00:32:32 +0200

    Copied in serialization code and added unit tests.

---
+++ SerializationTestProject/SerializationUnitTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+using System.IO;
+
+namespace SerializationTestProject
+{
+    [TestClass]
+    public class SerializationUnitTests
+    {
+        [TestMethod]
+        public void TestObjectSerialization()
+        {
+            //we use strings due to the dynamic size
+            string[] testStrings = new string[]
+            {
+                "This string seems to serialize fine",
+                "This string serializes fine",
+                $"Apparently this won't work with Siverlight:{Environment.NewLine}So apparently Silverlight does not have the BinaryFormatter. However an open source project exists that may be able to provide similar functionality for you. It is called sharpSerializer. It will work with Silverlight and WP7.'"
+            };
+
+            
+            foreach (string data in testStrings)
+            {
+                Debug.Assert(data.Equals(SerializationUtils.SerializationUtils.DeserializeFromByteArray<string>(SerializationUtils.SerializationUtils.SerializeToByteArray (data))));
+                
+
+            }
+        }
+
+        [TestMethod]
+        public void TestObjectSerializationToAndFromFile()
+        {
+            //we use strings due to the dynamic size
+            string[] testStrings = new string[]
+            {
+                "This string seems to serialize fine",
+                "This string serializes fine",
+                $"Apparently this won't work with Siverlight:{Environment.NewLine}So apparently Silverlight does not have the BinaryFormatter. However an open source project exists that may be able to provide similar functionality for you. It is called sharpSerializer. It will work with Silverlight and WP7.'"
+            };
+
+            int testNum = 0;
+            foreach (string data in testStrings)
+            {
+                ++testNum;
+
+                string temp = data;
+
+                //create a unique file name for the test, one that should not exist
+                string fileName = $"SerializationTestFileName{testNum}{DateTime.Now.Ticks}";
+
+                SerializationUtils.SerializationUtils.SerializeToFile<string>(fileName, ref temp);
+                string deserializedTemp = "";
+                Debug.Assert(SerializationUtils.SerializationUtils.DeserializeFromFile<string>(fileName, ref deserializedTemp));
+
+                Debug.Assert(data.Equals(deserializedTemp));
+
+                //delete this file
+                File.Delete(fileName);
+
+
+            }
+        }
+    }
+}
---
+++ SerializationUtils/SerializationUtils.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationUtils
+{
+    public class SerializationUtils
+    {
+        /// <summary>
+        /// Deserialize the data in file fileName and put it in the given object
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fileName"></param>
+        /// <param name="objectToLoadDataInto"></param>
+        /// <returns></returns>
+        public static bool DeserializeFromFile<T>(string fileName, ref T objectToLoadDataInto) where T : class
+        {
+            if (File.Exists(fileName))
+            {//first confirm that the file exists
+                using (Stream dataStream = File.OpenRead(fileName))
+                { //File.OpenRead opens the file with the read flag, rather than us having to set it. The using statement ensures that the created stream is cleaned up at the end of the block
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    objectToLoadDataInto = ((formatter.Deserialize(dataStream) as T));
+                    return true;
+                }
+                //return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Serialize the given object to the file named fileName
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fileName"></param>
+        /// <param name="objectToLoadDataInto"></param>
+        public static void SerializeToFile<T>(string fileName, ref T objectToLoadDataInto) where T : class
+        {
+            using (Stream stream = File.OpenWrite(fileName /*+ "_"+ ((int)(System.DateTime.Now.ToOADate()*1000))+ ".qsf"*/)) //en sure that stream is cleaned up by creating it in a using statement - the stream will be cleaned up at the end of the using block
+            {
+                //Debug.Log("Serializing tree!");
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, objectToLoadDataInto);
+            }
+        }
+
+        /// <summary>
+        /// For given object, serialize it and return the byte array
+        /// containing the serialized data
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static byte[] SerializeToByteArray<T>(T obj)
+        {
+            if (obj == null)
+            {
+                return null; //prevent errors to do with trying to serialize null
+            }
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, obj);
+                return ms.ToArray();
+            }
+        }
+
+        
+        /// <summary>
+        /// For a given byte array, attemts to deserialize it to an object of the given
+        /// type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static T DeserializeFromByteArray<T>(byte[] data)
+        {
+            if (data == null)
+            {
+                return default(T); //prevent errors to do with deserialing a null byte array
+            }
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                object obj = bf.Deserialize(ms);
+                return (T)obj;
+            }
+        }
+    }
+}
